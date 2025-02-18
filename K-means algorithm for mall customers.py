014c8a48e2a2c2f762168396bd50d6561cfedd19
Jan Saida Shaik <129920748+JansaidaA2@users.noreply.github.com>
2025-02-18 20:33:26 +0530

    Add files via upload

---
+++ K-means algorithm for mall customers.py
@@ -0,0 +1,62 @@
+# ------------------------ k-means algorithm ----------------------- #
+
+# importing libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# importing dataset 
+
+dataset=pd.read_csv(r"C:\Users\Jan Saida\OneDrive\Documents\Desktop\Excel sheets\Mall_Customers.csv")
+x=dataset.iloc[:,[3,4]].values
+
+# using elbow method to find the optimal number of clusters
+
+from sklearn.cluster import KMeans
+
+wcss=[]
+
+for i in range(1,11):
+    kmeans=KMeans(n_clusters=i,init="k-means++",random_state=0)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1,11),wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number Of Clusters')
+plt.ylabel('WCSS')
+plt.show()
+# wcss we have very good parameter called inertia_ credit goes to sklearn , that computes the sum of square , formula it will compute
+
+# training the K-Means model on the dataset
+
+kmeans=KMeans(n_clusters=5,init='k-means++',random_state=0)
+y_kmeans=kmeans.fit_predict(x)
+
+# visualising the clusters
+
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(x[y_kmeans == 3, 0], x[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(x[y_kmeans == 4, 0], x[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
+
+y_kmeans=kmeans.labels_
+dataset['cluster']=y_kmeans
+
+# pickling the file
+
+import pickle
+filename='Mall_prediction.pkl'
+with open (filename,'wb') as file:
+    pickle.dump(kmeans,file)
+print('Model has been saved and Pickled as Mall_prediction.pkl')
+
+import os 
+os.getcwd()
